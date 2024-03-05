9a5e03068bbeb057e88726b63102e994e5c0d2d0
Ian Blanco <ian.lautaro19@gmail.com>
2024-03-05 00:10:31 -0300

    modificaciones 4/3

--- js/index.js
+++ js/index.js
@@ -1,149 +1,117 @@
-let userPrev;
-let user = () => {
-    let userN = prompt("Ingrese su nombre se usuario");
+//Productos
 
-    let passN = prompt(userN + " ingrese su contraseña");
-    let passCheck = prompt(userN + " vuelva ingresar su contraseña");
-
-    if (passN != passCheck) {
-
-        do {
-            alert("No coinciden vuelva a intentarlo");
-            passN = prompt(userN + " ingrese su contraseña nuevamente");
-            passCheck = prompt(userN + " vuelva ingresar su contraseña");
-        } while (passN != passCheck)
-    }
-
-    alert("Las contraseñas coinciden ✔")
-    alert("Bienvendo/a " + userN)
-    return userPrev = userN;
+function Producto(id, nombre, imagen, valor, unidades, personalizada) {
+  this.id = id;
+  this.nombre = nombre;
+  this.imagen = imagen;
+  this.valor = valor;
+  this.unidades = unidades;
+  this.personalizada = personalizada;
 }
 
+// lista de productos
 
-let user_1;
-
-let nuevoUsuario = () => {
-
-    let newUser = prompt("Ingrese Nombre de usuario");
-    let newUserPass = prompt("Ingrese su contraseña, la misma deben ser 4 numeros");
-    alert("Bienvenido/a! \nSu usuario: " + newUser + "\nSu contraseña: " + newUserPass);
-    return user_1 = newUser;
-}
-
-
-
-let registrado = confirm("Bienvenido/a! estas registado en ''Badolato Dulces''?")
-
-console.log(registrado);
-
-if (registrado) {
-    user();
-} else {
-    nuevoUsuario()
-}
-
-
-
-
-const PRODUCTOS = [{
-        nombre: "Torta",
-        valor: 10000,
-        unidades: 1,
-        personalizada: false,
-    },
-    {
-        nombre: "Galletitas",
-        valor: 6000,
-        unidades: 5,
-        personalizada: false,
-    },
-    {
-        nombre: "Cupcake",
-        valor: 5000,
-        unidades: 2,
-        personalizada: false,
-    },
-    {
-        nombre: "Alfajores",
-        valor: 2000,
-        unidades: 3,
-        personalizada: false,
-    }
+const PRODUCTOS = [
+  new Producto(1, "Torta", "multimedia/prod_1.jpg", 10000, 1, false),
+  new Producto(2, "Galletitas", "multimedia/prod_2.jpg", 1500, 1, false),
+  new Producto(3, "Cupcakes", "multimedia/prod_3.jpg", 2500, 1, false),
+  new Producto(4, "Alfajores", "multimedia/prod_4.jpg", 1200, 1, false)
 ];
 
+let contenedorProductos = document.getElementById("contenedorProductos");
 
+// Crear  HTML => CARD
+let cardHtml = PRODUCTOS.map(function (PRODUCTOS) {
 
-let mostrarProductos = "";
-for (let i = 0; i < PRODUCTOS.length; i++) {
-    mostrarProductos += (PRODUCTOS[i].nombre + " " + PRODUCTOS[i].unidades + "u : $" + PRODUCTOS[i].valor + "\n")
-};
-alert("Menu: \n" + mostrarProductos);
+  return `
+      <div class="card">
+        <h3>${PRODUCTOS.nombre}</h3>
+        <img src="${PRODUCTOS.imagen}" alt="${PRODUCTOS.nombre}">
+        <p>Valor: $${PRODUCTOS.valor}</p>
+        <p>Unidades: ${PRODUCTOS.unidades}</p>
+        <button class="botonAgregar" data-id="${PRODUCTOS.id}">Agregar</button>
+      </div>
+    `;
+    
+});
 
+contenedorProductos.innerHTML = cardHtml.join('');
+
+//Carrito:
 
 const CARRITO = [];
 
+let contenedorCarrito = document.getElementById("contenedorCarrito");
 
-let agregarProductos = () => {
-    let salir;
-    while (salir != false) {
-        let pedir = parseFloat(prompt("Ingrese el numero correspondiente a la opcion deseada\nBadolato dulces\nCatalogo: \n1) Torta: $10.000\n2) Galletitas x5: $6.000\n3) Cupcake x2: $5000\n4) Alfajores x3: $2.000\n "));
-        switch (pedir) {
-            case 1:
-                CARRITO.push(PRODUCTOS[0])
-                break;
-            case 2:
-                CARRITO.push(PRODUCTOS[1])
-                break;
-            case 3:
-                CARRITO.push(PRODUCTOS[2])
-                break;
-            case 4:
-                CARRITO.push(PRODUCTOS[3])
-                break;
-            default:
-                alert("Opcion incorrecta.");
+function totalCarrito() {
 
-        }
-        salir = confirm("Desea sumar otro producto a su pedido?")
+  let total = 0;
+  CARRITO.forEach(producto => {
+    total += producto.valor * producto.unidades;
+  });
+
+  return total;
+
+}
+
+function totalHtml() {
+  const TOTAL_HTML = document.getElementById("totalHtml");
+  TOTAL_HTML.textContent = `$${totalCarrito()}`;
+}
+
+function agregarAlCarrito(PRODUCTOS) {
+
+  const INDICE = CARRITO.findIndex(item => item.id === PRODUCTOS.id);
+
+  INDICE !== -1 ? CARRITO[INDICE].unidades += 1 : CARRITO.push(PRODUCTOS);
+
+  totalCarrito();
+
+  // Crear  HTML => CARD
+  let cardCarritoHtml = CARRITO.map(function (producto) {
+
+    return `
+      <div class="card">
+        <h3>${producto.nombre}</h3>
+        <img src="${producto.imagen}" alt="${producto.nombre}">
+        <p>Valor: $${producto.valor * producto.unidades}</p>
+        <p>Unidades: ${producto.unidades}</p>
+        <button class="botonQuitar" data-id="${producto.id}">Quitar</button>
+      </div>
+    `;
+
+  });
+  contenedorCarrito.innerHTML = cardCarritoHtml.join('');
+
+  totalHtml()
+}
+
+let botonAgregar = document.querySelectorAll(".botonAgregar");
+
+botonAgregar.forEach(function (boton) {
+  boton.addEventListener('click', function () {
+
+    let idProducto = parseInt(boton.getAttribute('data-id'));
+    let prod = PRODUCTOS.find(function (producto) {
+      return producto.id === idProducto;
+    });
+
+    if (prod) {
+      agregarAlCarrito(prod);
     }
-}
-agregarProductos();
 
+  });
+});
 
-let estadoCarrito = () => {
-    let mostrarCarrito = "";
-    let montoCarrito = 0;
+//Borrar 
 
-    for (let i = 0; i < CARRITO.length; i++) {
-        mostrarCarrito += (i + 1 + ") " + CARRITO[i].nombre + " " + CARRITO[i].unidades + "u : $" + CARRITO[i].valor + "\n");
-        montoCarrito += CARRITO[i].valor;
-    };
-    alert("Su compra es\n" + mostrarCarrito + "\n Monto total:" + montoCarrito);
-}
+function vaciarCarrito() {
 
-estadoCarrito();
-
-let ok_compra = confirm("Confirma el pedido?")
-
-let modificacion = () => {
-    let user_mod = parseFloat(prompt("Para agregar otro producto presione => 1\n Para borrar un producto presione => 2"));
-    if (user_mod == 1) {
-        agregarProductos();
-
-    } else if (user_mod == 2) {
-        let borrarProducto = parseFloat(prompt("Que producto desea borrar? Indique su numero."))
-        let q = borrarProducto - 1;
-        CARRITO.splice(q, 1)
-    } else {
-        alert("Opcion incorrecta.")
-    }
-}
-
-while (ok_compra != true) {
-
-    modificacion();
-    estadoCarrito();
-    ok_compra = confirm("Confirma el pedido?")
+  CARRITO.length = 0;
+  contenedorCarrito.innerHTML = '';
+  totalHtml();
 
 }
-alert("Gracias volve pronto!")
+
+const VACIAR_CARRITO = document.getElementById("borrar");
+VACIAR_CARRITO.addEventListener('click', vaciarCarrito);
